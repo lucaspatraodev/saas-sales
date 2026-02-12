4a42ecf069e04a1f57da00e8d1a335b9410387b7
Lucas de Castro <lucasdecastropatrao@gmail.com>
2026-02-11 21:37:40 -0300

    feat(auth): laravel Sanctum API

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\TenantController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\CustomerController;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+use App\Http\Controllers\Api\AuthController;
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::apiResource('tenants', TenantController::class)->only(['index', 'store']);
+    Route::apiResource('products', ProductController::class);
+    Route::apiResource('customers', CustomerController::class);
 });
